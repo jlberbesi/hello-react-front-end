09823159a97467a235e9d42ba777e580700e795c
Jlberbesi <jlberbesi.walloom@gmail.com>
2024-02-03 19:48:52 -0500

    Add fixes in app,js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Greeting from './Greeting'; // Asegúrate de que la ruta de importación sea correcta
+
 
 function App() {
   return (
